645961d48d7a9fae175bd6c3be9066de13596506
Zenkixz <senjixz@gmail.com>
2026-02-05 09:05:44 +0700

    com108-cs21301-lab6-AnhQuan

--- bai2.c
+++ bai2.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [NGUYỄN HUỲNH ANH QUÂN]
+ * MSSV:      [PS48285]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
 //  BÀI 2: TÌM GIÁ TRỊ LỚN NHẤT VÀ NHỎ NHẤT TRONG MẢNG 
@@ -10,3 +10,32 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+
+    int mang[n];
+    for(i = 0; i < n; i++) {
+        printf("Nhap phan tu thu %d: ", i + 1);
+        scanf("%d", &mang[i]);
+    }
+
+    int max = mang[0];
+    int min = mang[0];
+    for(i = 1; i < n; i++) {
+        if(mang[i] > max) {
+            max = mang[i];
+        }
+        if(mang[i] < min) {
+            min = mang[i];
+        }
+    }
+
+    printf("Gia tri lon nhat trong mang la: %d\n", max);
+    printf("Gia tri nho nhat trong mang la: %d\n", min);
+
+    return 0;
+}
