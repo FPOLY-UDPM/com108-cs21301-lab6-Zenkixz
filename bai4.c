06523ecca1621f4dad0b3a343b7ff8d400517529
Zenkixz <senjixz@gmail.com>
2026-02-05 09:10:46 +0700

    com108-cs21301-lab6-AnhQuan

--- bai4.c
+++ bai4.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [NGUYỄN HUỲNH ANH QUÂN]
+ * MSSV:      [PS48285]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
 //  BÀI 4: TÍNH BÌNH PHƯƠNG CÁC PHẦN TỬ TRONG MẢNG 2 CHIỀU 
@@ -10,3 +10,30 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+
+int main() {
+    int n, m, i, j;
+    printf("Nhap so hang cua ma tran: ");
+    scanf("%d", &n);
+    printf("Nhap so cot cua ma tran: ");
+    scanf("%d", &m);
+
+    int matrix[n][m];
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < m; j++) {
+            printf("Nhap phan tu tai vi tri [%d][%d]: ", i + 1, j + 1);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Ma tran binh phuong la:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < m; j++) {
+            printf("%d ", matrix[i][j] * matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
